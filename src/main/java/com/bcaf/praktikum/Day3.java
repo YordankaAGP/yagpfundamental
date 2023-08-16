97632d1fb3e5c5d07bc638522c560e6e75abfb7f
Yordanka AGP <yordankaandre@gmail.com>
2023-08-16 13:20:53 +0700

    Created day3 class

--- src/main/java/com/bcaf/praktikum/Day3.java
+++ src/main/java/com/bcaf/praktikum/Day3.java
@@ -1,7 +1,24 @@
 package com.bcaf.praktikum;
 
+import java.util.Scanner;
+
 public class Day3 {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter text: ");
+        String text = scan.nextLine();
 
+        char[] textArr = text.toCharArray();
+
+        int len = textArr.length;
+        int sum = 0;
+
+        for (int i=0; i < len; i++) {
+            if(textArr[i] >= 65 && textArr[i] <= 90 || textArr[i] >= 97 && textArr[i] <= 122) {
+                sum += textArr[i];
+            }
+        }
+
+        System.out.println(sum);
     }
 }
